502e017660840cfed3a294b8dfa3dc3ee0cc2ac2
devs <doc1@gmail.com>
2020-09-09 00:18:58 +1000

    update

---
+++ precache-manifest.acd3fdd45ef64e9eb6b9144a81577bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08eed3545d3ede28b0a0b71ae3a37a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dafd6755efb5a896be2",
+    "url": "/static/css/main.9bc6d313.chunk.css"
+  },
+  {
+    "revision": "94e8b71b7f34bcbcfa39",
+    "url": "/static/js/2.3aa7f538.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.3aa7f538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dafd6755efb5a896be2",
+    "url": "/static/js/main.aaceae09.chunk.js"
+  },
+  {
+    "revision": "1a684650522a3ca04aa2",
+    "url": "/static/js/runtime-main.75d8b0e8.js"
+  },
+  {
+    "revision": "cc47ad768cad9f1aec91099ad41b588a",
+    "url": "/static/media/farmer.cc47ad76.png"
+  }
+]);
